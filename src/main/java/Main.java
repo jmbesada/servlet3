02bee2c2a87b2a3ec977f0feab1f7b0f28c702d3
jose miguel <jose miguel@192.168.0.192>
2012-03-18 14:23:41 +0100

    Import inicial

---
+++ src/main/java/Main.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Main {
+
+	public static void main(String[] args){
+		String a="aaa";
+		switch (a){
+			case "ss":{
+				System.out.println("1");break;
+				}
+			case "aaa":{
+					System.out.println("2");break;
+				}
+		}
+		
+		Long creditCard=2222_2222_22_3333333l;
+		System.out.println(creditCard);
+		List<String> list=new ArrayList<>();
+		list.add("aaa");
+	}
+	
+}
